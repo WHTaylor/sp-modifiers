83d6c6fed9895ed2ae08cbea5d6bf80894a6f53b
Will Taylor <william.h.taylor@stfc.ac.uk>
2019-02-14 17:32:45 +0000

    Combine modifed data from all downloaded lists

---
+++ modified_data_combiner.py
@@ -0,0 +1,54 @@
+import os.path
+import json
+from datetime import datetime
+from main import order_by_int_value, get_replacement_names
+
+
+def combine_cached_data():
+    combined = dict()
+    cache_folder = os.path.join(os.curdir, "data", "cache")
+    for filename in os.listdir(cache_folder):
+        with open(os.path.join(cache_folder, filename), "r") as input_file:
+            data = json.load(input_file)
+            for entry in data:
+                date = datetime.strptime(entry["Modified"], "%Y-%m-%dT%H:%M:%S")
+                if date > datetime(2017, 1, 1):
+                    if entry["ModifiedById"] in combined:
+                        combined[entry["ModifiedById"]] += 1
+                    else:
+                        combined[entry["ModifiedById"]] = 1
+    ordered = order_by_int_value(combined)
+    translated_names = translate_names(ordered)
+    for name, count in translated_names:
+        print(name, count)
+
+
+def translate_names(modifier_counts):
+    named_modifiers = replace_ids_with_names(modifier_counts)
+    known_names = get_replacement_names()
+    translated = []
+    for name, count in named_modifiers:
+        replacement_name = name
+        if name in known_names:
+            replacement_name = known_names[name]
+
+        replacement_name = replacement_name.replace("(STFC,RAL,ISIS)", "")
+        replacement_name = replacement_name.replace("(STFC,RAL,CLF)", "")
+        translated.append((replacement_name, count))
+
+    return translated
+
+
+def replace_ids_with_names(modifier_counts):
+    ids = [id for (id, _) in modifier_counts]
+    names = dict()
+    with open(os.path.join(os.curdir, "data", "sp_users.json")) as input_file:
+        data = json.load(input_file)
+        for entry in data:
+            if entry["Id"] in ids:
+                names[entry["Id"]] = entry["Name"]
+    return [(names[id], count) for (id, count) in modifier_counts]
+
+
+if __name__ == "__main__":
+    combine_cached_data()
